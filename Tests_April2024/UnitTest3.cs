b37cdb6fe0d525e95b94b54f8fca1d361f2f707d
MAK7755 <162543195+MAK7755@users.noreply.github.com>
2024-11-10 21:29:03 +0330

    Update UnitTest3.cs

--- Tests_April2024/UnitTest3.cs
+++ Tests_April2024/UnitTest3.cs
@@ -6,14 +6,13 @@ using System.Threading.Tasks;
 
 namespace Tests_April2024
 {
-    [TestFixture]
     public class Form1_Tests
     {
         //Author: Mohammad Kiani Sheikhabadi
 
         /*Description: This function checks if button2_Click method 
          is working properly*/
-        [Test]
+        [Fact]
         public void Button2_Click_Check()
         {
             Form1 form = new Form1();
